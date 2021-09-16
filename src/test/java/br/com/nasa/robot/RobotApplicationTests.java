186470bab7cb2dbf807cd01b5999ff93bf3d18e6
hackerrank <me@hackerrank.com>
2021-09-16 11:23:21 -0300

    correção na instância do service.

--- src/test/java/br/com/nasa/robot/RobotApplicationTests.java
+++ src/test/java/br/com/nasa/robot/RobotApplicationTests.java
@@ -2,6 +2,7 @@ package br.com.nasa.robot;
 
 import br.com.nasa.robot.service.RobotService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class RobotApplicationTests {
 
+    @Autowired
     private RobotService robotService;
 
     @Test
